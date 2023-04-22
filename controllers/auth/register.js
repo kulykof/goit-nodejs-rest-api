2bb1e580b40e4e3dbbee159baa1370320030ece8
Oleksandr <kulykof@gmail.com>
2023-04-22 12:41:57 +0300

    add some changes

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt');
 const { Conflict } = require('http-errors');
 
+const gravatar = require('gravatar');
+const { nanoid } = require('nanoid');
+
 const { User } = require('../../models/user');
 
 const register = async (req, res, next) => {
@@ -12,10 +15,14 @@ const register = async (req, res, next) => {
             throw new Conflict('Email in use');
         }
 
+        const avatarURL = gravatar.url(email); 
         const hashPassword = await bcrypt.hash(password, 10);
+        const verificationToken = nanoid();
         const newUser = await User.create({
             ...req.body,
             password: hashPassword,
+            avatarURL,
+            verificationToken,
         });
 
         res.status(201).json({
